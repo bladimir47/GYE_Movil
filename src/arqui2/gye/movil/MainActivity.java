4bc984ea42271c6659a64965928e77d2de8ac911
Pablo David Romero <pablo_david_vero@hotmail.com>
2014-04-16 22:08:43 -0600

    grafico hasta menu principal

--- src/arqui2/gye/movil/MainActivity.java
+++ src/arqui2/gye/movil/MainActivity.java
@@ -6,6 +6,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -14,8 +16,22 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void NuevoMetodo(){
-    	
+    public void IniciarSesion(View boton){
+    	boolean pasa=true;
+    	TextView txtuser = (TextView)findViewById(R.id.txtUsuario_Login);
+    	TextView txtpass = (TextView)findViewById(R.id.txtPass_Login);
+    	String usuario="",pass="";
+    	try{
+    		usuario=txtuser.getText().toString();
+    		pass=txtpass.getText().toString();
+    		if(pasa){
+    			Intent intent=new Intent();
+    			intent.setClass(this, MenuPrincipal.class);
+    			startActivity(intent);
+    		}
+    	}catch(Exception ex){
+    		Toast.makeText(this, "los campos no pueden estar vacios", Toast.LENGTH_SHORT);
+    	}	
     }
     public void IrRegistro(View boton){
     	Intent intent=new Intent();
